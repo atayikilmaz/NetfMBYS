9d217daa3f6e39da26d0f8de9cc1b69ab9fefb0e
Mahmut Ersoy <mahmut159ersoy@gmail.com>
2023-01-07 17:57:45 +0300

    form

--- NetfyMBYS/NetfyMBYS/Form1.Designer.cs
+++ NetfyMBYS/NetfyMBYS/Form1.Designer.cs
@@ -29,13 +29,54 @@ namespace NetfyMBYS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.doktorAdText = new System.Windows.Forms.TextBox();
+            this.sifreText = new System.Windows.Forms.TextBox();
+            this.girisBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // doktorAdText
+            // 
+            this.doktorAdText.Location = new System.Drawing.Point(280, 157);
+            this.doktorAdText.Name = "doktorAdText";
+            this.doktorAdText.Size = new System.Drawing.Size(100, 22);
+            this.doktorAdText.TabIndex = 0;
+            // 
+            // sifreText
+            // 
+            this.sifreText.Location = new System.Drawing.Point(280, 213);
+            this.sifreText.Name = "sifreText";
+            this.sifreText.Size = new System.Drawing.Size(100, 22);
+            this.sifreText.TabIndex = 1;
+            // 
+            // girisBtn
+            // 
+            this.girisBtn.Location = new System.Drawing.Point(497, 322);
+            this.girisBtn.Name = "girisBtn";
+            this.girisBtn.Size = new System.Drawing.Size(75, 23);
+            this.girisBtn.TabIndex = 2;
+            this.girisBtn.Text = "Giriş Yap";
+            this.girisBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.girisBtn);
+            this.Controls.Add(this.sifreText);
+            this.Controls.Add(this.doktorAdText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox doktorAdText;
+        private System.Windows.Forms.TextBox sifreText;
+        private System.Windows.Forms.Button girisBtn;
     }
 }
 
